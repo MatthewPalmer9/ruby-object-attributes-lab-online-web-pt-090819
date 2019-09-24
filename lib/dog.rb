12a35db2a8e351d703ad74612641a8395cb406a8
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-24 22:36:18 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name=(new_dog_name)
+    @name = new_dog_name 
+  end 
+  
+  def name 
+    p @name 
+  end 
+  
+  def breed=(breed_type)
+    @dog_breed = breed_type
+  end 
+  
+  def breed 
+    p dog_breed
+  end 
+  
+end 
